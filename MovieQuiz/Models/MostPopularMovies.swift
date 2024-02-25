2bbf10429ffa52a06e849574be34886b38d0c023
Simon <mocki.toki@gmail.com>
2024-02-25 18:08:53 +0300

    installed swiftlint

---
+++ MovieQuiz/Models/MostPopularMovies.swift
@@ -0,0 +1,8 @@
+//
+//  MostPopularMovies.swift
+//  MovieQuiz
+//
+//  Created by Simon Butenko on 25.02.2024.
+//
+
+import Foundation
---
+++ MovieQuiz/Services/NetworkClient.swift
@@ -0,0 +1,8 @@
+//
+//  NetworkClient.swift
+//  MovieQuiz
+//
+//  Created by Simon Butenko on 25.02.2024.
+//
+
+import Foundation
